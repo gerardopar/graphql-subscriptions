2034a56d70da855b65b208ac5301f0b48f3e0189
Gerardo Paredes <gerardoparedes@gerardos-mbp.home>
2019-04-03 11:21:43 -0700

    initial commit

--- src/myModule.js
+++
@@ -1,8 +0,0 @@
-const name = "Gerardo Paredes"
-const city = "Los Angeles";
-
-const currentOccupation = (job) => {
-    return `current occupation ${job}`;
-};
-
-export { name as default, city, currentOccupation };
