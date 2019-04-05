fbac3c0ded17becd7ad4ba07069d2f2288fd3916
Gerardo Paredes <gerardoparedes@gerardos-mbp.home>
2019-04-04 17:04:42 -0700

    count subscription

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // importing modules
-import { GraphQLServer } from 'graphql-yoga'; // * graphql server
+import { GraphQLServer, PubSub } from 'graphql-yoga'; // * graphql server
 
 // importing demo data
 import db from './db';
@@ -7,9 +7,12 @@ import db from './db';
 // importing resolvers
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
+import Subscription from './resolvers/Subscription';
 import User from './resolvers/User';
 import Post from './resolvers/Post';
 import Comment from './resolvers/Comment';
+
+const pubsub = new PubSub();
 
 // ! new server instance
 const server = new GraphQLServer({
@@ -17,12 +20,14 @@ const server = new GraphQLServer({
     resolvers: {
         Query,
         Mutation,
+        Subscription,
         User,
         Post,
         Comment
     },
     context: { // global data initialized
-        db: db
+        db: db,
+        pubsub
     }
 });
 
